ef98621eb937438ddc8a4b7a270476f25009fcf2
Shijin2k3 <shijinkumar2003@gmail.com>
2025-02-14 20:45:21 +0530

    feat: Added new Employee

--- Backend/controllers/employeeController.js
+++ Backend/controllers/employeeController.js
@@ -1,6 +1,15 @@
+const Employee=require('../models/employeeModel')
+
 exports.getEmployees= async(req,res,next)=>{
     res.status(200).json({
         success:true,
         message:"Employee API working successfully"
     })
+}
+exports.newEmployee= async(req,res,next)=>{
+   const employee=await Employee.create(req.body);
+   res.status(201).json({
+    success:true,
+    employee
+   })
 }
--- Backend/routes/employee.js
+++ Backend/routes/employee.js
@@ -1,7 +1,8 @@
 const express=require('express');
-const { getEmployees } = require('../controllers/employeeController');
+const { getEmployees, newEmployee } = require('../controllers/employeeController');
 const router=express.Router();
 
 router.route('/employees').get(getEmployees);
+router.route('/employee/new').post(newEmployee);
 
 module.exports=router;
