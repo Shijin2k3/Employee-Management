63fb54fa4a5575b6dba80ad3ecf97f777a827970
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-18 20:47:50 +0530

    Initialized RegisterUser

--- Backend/app.js
+++ Backend/app.js
@@ -4,9 +4,11 @@ const errorMiddleware=require('./middlewares/error')
 
 app.use(express.json()); 
 const employees=require('./routes/employee')
+const auth=require('./routes/auth')
 
 
 app.use('/api/v1/',employees)
+app.use('/api/v1/',auth)
 
 app.use(errorMiddleware);
 
---
+++ Backend/controllers/authController.js
@@ -0,0 +1,17 @@
+const catchASyncError=require('../middlewares/catchAsyncError')
+const User=require('../models/userModel')
+
+exports.registerUser=catchASyncError(async(req,res,next)=>{
+
+    const {name,email,password}=req.body;
+    const user= await User.create({
+        name,
+        email,
+        password
+    })
+    res.status(201).json({
+        success:true,
+        user
+    })
+
+})
