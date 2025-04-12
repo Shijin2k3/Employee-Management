ce8a66459b4b4087df42c79aed90073641e0ec2a
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-12 20:17:52 +0530

    Initialized admin routes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express=require('express');
-const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile, changePassword, updateProfile } = require('../controllers/authController');
-const { isAuthenticatedUser } = require('../middlewares/authenticate');
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile, changePassword, updateProfile, getAllUsers, getUser, deleteUser, updateUser } = require('../controllers/authController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
 const router=express.Router();
 
 router.route('/register').post(registerUser);
@@ -13,6 +13,11 @@ router.route('/password/change').put(isAuthenticatedUser,changePassword);
 router.route('/update').put(isAuthenticatedUser,updateProfile)
 
 //Admin routes
-router.route('/admin/users').get(isAuthenticatedUser,)
+router.route('/admin/users').get(isAuthenticatedUser,authorizeRoles('admin'),getAllUsers)
+router.route('/admin/users/:id').get(isAuthenticatedUser,authorizeRoles('admin'),getUser)
+router.route('/admin/users/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateUser)
+router.route('/admin/users/:id').delete(isAuthenticatedUser,authorizeRoles('admin'),deleteUser)
+
+
 
 module.exports=router;
