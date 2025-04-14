9c4d23f926cf1e528f84a8a4adf19fde2a012812
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-14 10:43:06 +0530

    Initialized header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Header } from './components/layouts/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-      </div>
-      <p className="text-blue-500">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header/>
+    </div>
+    
   )
 }
 
---
+++ frontend/src/components/layouts/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [activeLink, setActiveLink] = useState('home');
+
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+      };
+
+    return (
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-white text-lg font-bold">EMS</div>
+          <div className="hidden md:flex space-x-4 gap-6">
+            <a href="#"  
+            onClick={() => handleLinkClick('home')}
+            className={`text-white ${activeLink === 'home' ? 'font-bold' : 'hover:text-gray-400'}`}
+             >Home</a>
+            <a href="#"  
+            onClick={() => handleLinkClick('about')}
+            className={`text-white ${activeLink === 'about' ? 'font-bold' : 'hover:text-gray-400'}`}
+            >About</a>
+            <a href="#"  onClick={() => handleLinkClick('login')}
+            className={`text-white ${activeLink === 'login' ? 'font-bold' : 'hover:text-gray-400'}`}
+             >Login</a>
+            <a href="#"  onClick={() => handleLinkClick('contact')}
+            className={`text-white ${activeLink === 'contact' ? 'font-bold' : 'hover:text-gray-400'}`}
+            >Contact</a>
+          </div>
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="text-white focus:outline-none cursor-pointer">
+              {isOpen ?<AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+            </button>
+          </div>
+        </div>
+        {isOpen && (
+          <div className="md:hidden py-4">
+            <a href="#" className="block text-gray-300 hover:text-white py-3">Home</a>
+            <a href="#" className="block text-gray-300 hover:text-white py-3">About</a>
+            <a href="#" className="block text-gray-300 hover:text-white py-3">Login</a>
+            <a href="#" className="block text-gray-300 hover:text-white py-3">Contact</a>
+          </div>
+        )}
+      </nav>
+    );
+  
+}
