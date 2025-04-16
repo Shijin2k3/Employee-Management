faa912e7e527177148f572b50afb058868f97852
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-16 11:19:44 +0530

    Initialized authSlice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import  {thunk}  from "redux-thunk";
 import employeesReducer from './slices/employeesSlice'
+import authReducer from'./slices/authSlice';
 
 //can create multiple state
 const reducer = combineReducers({
-   employeesState:employeesReducer
+   employeesState:employeesReducer,
+   authState:authReducer
 })
 
 
