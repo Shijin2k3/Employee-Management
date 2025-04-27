8dc09a5328dcc356f5ecfa27a0b185924af0d48b
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-27 11:10:18 +0530

    Initialized Dynamic content To profile

--- frontend/src/components/user/Profile.jsx
+++ frontend/src/components/user/Profile.jsx
@@ -1,41 +1,43 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 
 export const Profile = () => {
-    const employee = {
-        name: "John Doe",
-        position: "Software Engineer",
-        email: "johndoe@example.com",
-        phone: "+1 234 567 890",
-        department: "Engineering",
-        profilePicture: "https://via.placeholder.com/150",
-        bio: "A passionate software engineer with 5 years of experience in building scalable web applications."
-      };
+    const {user} = useSelector(state => state.authState);
+   
     
       return (
         <div className="w-[100%] min-h-screen mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
           <div className="flex flex-col items-center p-6">
             <div className="mt-4   text-center ">
-              <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">{employee.name}</h1>
-              <p className="text-gray-600  md:text-xl">{employee.position}</p>  
+              <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4 uppercase">{user.name}</h1>
+              <p className="text-gray-600  md:text-xl">{user.position}</p>  
             </div>
             <div className='flex items-center p-4  justify-end'>
           <button className=' bg-blue-600 text-white w-30 h-8 rounded-lg'>Edit Profile</button>
           </div>
           </div>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap:2 md:gap-4 m-3 md:m-8'>
-            <div className="p-8 border-1">
-            <h2 className="text-lg font-semibold">About Me</h2>
-            <p className="text-gray-700">{employee.bio}</p>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-5 m-3 md:m-8'>
+            <div className="p-8 bg-slate-100 shadow-lg">
+            <h2 className="text-lg font-semibold">Position</h2>
+            <p className="text-gray-700">{user.Position}</p>
           </div>
-          <div className="p-8 border-1">
+          <div className="p-8 shadow-lg bg-slate-100">
             <h2 className="text-lg font-semibold">Department</h2>
-            <p className="text-gray-700">{employee.department}</p>
+            <p className="text-gray-700">{user.department}</p>
           </div>
-          <div className="p-8 border-1">
+          <div className="p-8 shadow-lg bg-slate-100">
             <h2 className="text-lg font-semibold">Email Adress</h2>
-            <p className="text-gray-700">{employee.email}</p>
+            <p className="text-gray-700">{user.email}</p>
           </div>
-          <div className="p-8 border-1">
+          <div className="p-8 shadow-lg bg-slate-100">
+            <h2 className="text-lg font-semibold">Address</h2>
+            <p className="text-gray-700">{user.address}</p>
+          </div>
+          <div className="p-8 shadow-lg bg-slate-100">
+            <h2 className="text-lg font-semibold">Joined</h2>
+            <p className="text-gray-700">{String(user.createdAt).substring(0,10)}</p>
+          </div>
+          <div className="p-8 shadow-lg bg-slate-100">
             <h2 className="text-lg font-semibold">Change Password</h2>
           </div>
           </div>
