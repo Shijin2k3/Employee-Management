52408670923de7442cde2f21724c94b72f68ba0c
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-30 20:14:00 +0530

    bug fix

--- frontend/src/components/user/UpdateProfile.jsx
+++ frontend/src/components/user/UpdateProfile.jsx
@@ -1,21 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const UpdateProfile = () => {
+  const {loading,error,user,isUpdate}=useSelector(state => state.authState);
+  const [name,setName]=useState("")
+  const [email,setEmail]=useState("")
+  const dispatch=useDispatch()
+
+  const submitHandler=()=>{
+    
+  }
   return (
     <Fragment>
            <div className=' bg-gradient-to-r from-blue-950 to-blue-900'>
                <div className='flex flex-col items-center justify-center
                  spacey-6 h-screen'>
-                   <form 
-                    className='shadow-lg p-6 w-65 md:w-80  bg-white'>
+                   <form onSubmit={submitHandler}
+                    className='shadow-lg p-6 w-65 md:w-80 py-10 bg-white'>
                        <h1 className='mb-4 text-2xl font-bold '>Update Profile</h1>
-                       <div className='mb-4'> 
+                       <div className='mb-4 '> 
                            <label htmlFor="name"
                             className='block text-gray-700'>Name</label>
                            <input type="text"
                            name='name'
                            id='name'
                            className='w-full border-2 border-gray-500'
+                            value={name}
+                            onChange={e=>setName(e.target.value)}
                             />
                        </div>
                        <div className='mb-4'> 
@@ -25,10 +36,12 @@ export const UpdateProfile = () => {
                            name='email'
                            id='email'
                            className='w-full border-2 border-gray-500'
-                            />
+                           value={email}
+                           onChange={e=>setEmail(e.target.value)}
+                           />
                        </div>
                        
-                       <div className='mb-4'>
+                       <div className='mb-4 mt-10'>
                         <button  type='submit' className='w-full bg-blue-900 py-2 mt-2 text-white'
                          >Update Profile</button>
                        </div>
