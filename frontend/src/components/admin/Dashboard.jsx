a810e5cf53134d481bd20fdef7f32119fcd7c06b
Shijin2k3 <shijinkumar2003@gmail.com>
2025-05-13 10:27:59 +0530

    Initialized styling in side bar

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Link } from 'react-router-dom'
 
 const Dashboard = () => {
+  const [activeLink,setActiveLink]=useState('dashboard')
+
+  const handleLinkClick=(link)=>{
+    setActiveLink(link)
+  }
+
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
-      <div className="w-64 bg-blue-950 text-white p-6">
-        <h2 className="text-2xl font-bold mb-6">Admin Panel</h2>
-        <ul className="space-y-4">
-          <li className="hover:bg-blue-900 p-2 rounded-lg cursor-pointer">Overview</li>
-          <li className="hover:bg-blue-900 p-2 rounded-lg cursor-pointer">Employees</li>
-          <li className="hover:bg-blue-900 p-2 rounded-lg cursor-pointer">Reports</li>
-          <li className="hover:bg-blue-900 p-2 rounded-lg cursor-pointer">Settings</li>
+      <div className="w-64 bg-blue-950 text-white p-6 ">
+        <ul className="space-y-4 flex flex-col">
+          <Link to="/admin/dashboard"
+          onClick={()=>handleLinkClick('dashboard')}
+           className={`${activeLink === 'dashboard'?"bg-blue-900 rounded-lg":"bg-blue-950"} p-2 cursor-pointer`}>Dashboard</Link>
+          <Link to="/admin/dashboard" className="hover:bg-blue-900 p-2 rounded-lg cursor-pointer">Employees</Link>
+          <Link to="/admin/dashboard" className="hover:bg-blue-900 p-2 rounded-lg cursor-pointer">Reports</Link>
+          <Link to="/admin/dashboard" className="hover:bg-blue-900 p-2 rounded-lg cursor-pointer">Settings</Link>
         </ul>
       </div>
 
